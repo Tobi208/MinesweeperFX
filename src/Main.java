f20cc3c747ae173a1a8a7650badcb7c56a14e42c
tobi208 <tobias96.home@googlemail.com>
2021-01-18 09:44:00 +0100

    fxml setup

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/root.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/minesweeper.fxml"));
         primaryStage.setTitle("MinesweeperFX");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
---
+++ src/control/Control.java
@@ -0,0 +1,18 @@
+package control;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class Control {
+
+    public TextField rowsField;
+    public TextField colsField;
+    public TextField bombsField;
+    public TextField simulationsField;
+
+    public void simulate(ActionEvent actionEvent) {
+
+        System.out.println("hello :)");
+
+    }
+}
